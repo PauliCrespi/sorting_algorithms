9487866c63d13b8a07525165fe295c1b077b028b
PauliCrespi <4377@holberton.com>
2022-06-13 08:14:00 -0700

    task 0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -7,20 +7,20 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-        size_t i = 0, y = 0;
-        int aux;
+	size_t i = 0, y = 0;
+	int aux;
 
-        for (y = 0; y < size - 1; y++)
-        {
-                for (i = 0; i < size - y - 1; i++)
-                {
-                        if (array[i] > array[i + 1])
-                        {
-                                aux = array[i];
-                                array[i] = array[i + 1];
-                                array[i + 1] = aux;
-                                print_array(array, size);
-                        }
-                }
-        }
+	for (y = 0; y < size - 1; y++)
+	{
+		for (i = 0; i < size - y - 1; i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				aux = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = aux;
+				print_array(array, size);
+			}
+		}
+	}
 }
