0306975b193ad0a4cf42829faf7a92782da2cd49
PauliCrespi <4377@holberton.com>
2022-06-13 08:32:00 -0700

    task 2

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -8,10 +8,11 @@
 void selection_sort(int *array, size_t size)
 {
 	size_t i, j;
-	int aux, flag;
+	int aux, flag, yes;
 
 	for (j = 0; j < size - 1; j++)
 	{
+		yes = 0;
 		aux = array[j];
 		for (i = j; i < size; i++)
 		{
@@ -19,10 +20,14 @@ void selection_sort(int *array, size_t size)
 			{
 				aux = array[i];
 				flag = i;
+				yes = 1;
 			}
 		}
-		array[flag] = array[j];
-		array[j] = aux;
-		print_array(array, size);
+		if (yes == 1)
+		{
+			array[flag] = array[j];
+			array[j] = aux;
+			print_array(array, size);
+		}
 	}
 }
